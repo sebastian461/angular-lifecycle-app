8b4e116c25bd45ad832ede67488ba4ddd4cf6f7e
sebastian461 <sebastiano-461@hotmail.com>
2024-04-05 15:38:02 -0500

    Configuración inicial

---
+++ src/app/products/components/price/price.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'products-price-component',
+  templateUrl: './price.component.html',
+  styles: ``,
+})
+export class PriceComponent {}
